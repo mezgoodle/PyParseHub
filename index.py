ec28ec3eb435f96b903a60a51a779116cd370ad4
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-11 21:34:03 +0300

    Add headers and make function

--- index.py
+++ index.py
@@ -3,9 +3,19 @@ from bs4 import BeautifulSoup as BS
 from datetime import datetime
 
 username = 'mezgoodle'
+url = 'https://github.com/'
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Mobile Safari/537.36',
+    'accept': '*/*',
+}
 
-r = requests.get(f'https://github.com/{username}')
-html = BS(r.content, 'html.parser')
+
+def get_html(url, username):
+    r = requests.get('https://github.com/' + username, headers=HEADERS)
+    return BS(r.content, 'html.parser')
+
+
+html = get_html(url, username)
 
 for el in html.select('.js-calendar-graph-svg > g > g > .day'):
     date = el['data-date']
