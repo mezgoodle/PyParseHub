f9d860b01c0a9af3014ca3d80ecc76435431e385
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-11 12:38:14 +0300

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,6 @@ username = 'mezgoodle'
 
 r = requests.get(f'https://github.com/{username}')
 html = BS(r.content, 'html.parser')
+
+for el in html.select('.js-calendar-graph-svg'):
+    print(el.select('g'))
