b950b559cfb5b099866dd1c6dd219a57a9be8ba6
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-11 13:03:55 +0300

    Import datetime functions

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as BS
+from datetime import datetime
 
 username = 'mezgoodle'
 
@@ -7,6 +8,7 @@ r = requests.get(f'https://github.com/{username}')
 html = BS(r.content, 'html.parser')
 
 for el in html.select('.js-calendar-graph-svg > g > g > .day'):
-    class_ = el['data-date']
-    print(class_)
+    date = el['data-date']
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    print(date_obj, type(date_obj))
 # print(html.select('.js-calendar-graph-svg > g > g > .day')[-1]['class'])
